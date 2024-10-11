5304e8ac4628bf0aa83a0fa2f4275785c9267fd9
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-10-11 13:41:54 +0300

    When Open App Redirect To Main EnsembleEnabler Site

--- ensemble-enabler-webinar/src/components/Routes/Routes.jsx
+++ ensemble-enabler-webinar/src/components/Routes/Routes.jsx
@@ -1,7 +1,16 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
 import { InnovationScreen } from "../../screens";
+import { useEffect } from "react";
 
 const RoutesComp = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      window.location.href = "https://www.ensembleenabler.com/";
+    }
+  }, [location]);
+
   return (
     <>
       <Routes>
